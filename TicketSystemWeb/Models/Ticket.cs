d129a4638cdb121aea9e1509bdb738bc9108e218
Feller,Ruby R <ruby.feller@student.fontys.nl>
2022-03-07 10:21:33 +0100

    Translated validation errors to Dutch

--- TicketSystemWeb/Models/Ticket.cs
+++ TicketSystemWeb/Models/Ticket.cs
@@ -11,18 +11,18 @@ namespace TicketSystemWeb.Models
     {
         [Key]
         public int ticketId { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Het invullen van een onderwerp is verplicht")]
         [DisplayName("Onderwerp")]
         public string ticketSubject { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Het invullen van een probleemomschrijving is verplicht")]
         [DisplayName("Probleemomschrijving")]
         public string ticketContent { get; set; }
         public int displayOrder { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Het invullen van een categorie is verplicht")]
         public int ticketCategory { get; set; }
-        [Required]
-        public int ticketPriority { get; set; } 
-        [Required]
+        [Required(ErrorMessage = "Het invullen van een prioriteit is verplicht")]
+        public int ticketPriority { get; set; }
+        [Required(ErrorMessage = "Het invullen van een status is verplicht")]
         public int ticketStatus { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
