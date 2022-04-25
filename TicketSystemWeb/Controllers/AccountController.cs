9073a961fc0c5d1c6f5e169d0b04aaff6ae23346
Feller,Ruby R <ruby.feller@student.fontys.nl>
2022-04-25 16:56:27 +0200

    Started working on hard-coded login and different roles (employee and administrator)

---
+++ TicketSystemWeb/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TicketSystemWeb.ViewModels;
+
+namespace TicketSystemWeb.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Signup()
+        {
+            return View();
+        }
+        public IActionResult Login(EmployeeAccountViewModel obj)
+        {
+            if (ModelState.IsValid)
+            {
+
+                if (obj.Password == "test")
+                {
+                    TempData["employee"] = 1;
+                    TempData["success"] = "Succesvol ingelogd als medewerker";
+                    return RedirectToAction("Index", "Home");
+
+                }
+                if (obj.Password == "test2")
+                {
+                    TempData["administrator"] = 1;
+                    TempData["success"] = "Succesvol ingelogd als administrator";
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    TempData["error"] = "Niet ingelogd: controleer gegevens";
+                }
+            }
+            return RedirectToAction("Index", "Account");
+        }
+        public IActionResult Logout()
+        {
+            TempData.Remove("employee");
+            TempData.Remove("administrator");
+            TempData["success"] = "Succesvol uitgelogd";
+            return RedirectToAction("Index");
+        }
+    }
+}
--- TicketSystemWeb/ViewModels/EmployeeAccountViewModel.cs
+++ TicketSystemWeb/ViewModels/EmployeeAccountViewModel.cs
@@ -2,14 +2,14 @@
 
 namespace TicketSystemWeb.ViewModels
 {
-    public class EmployeeViewModel
+    public class EmployeeAccountViewModel
     {
         [Key]
         public int EmployeeId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Password { get; set; }
         public int CompetenceLevel { get; set; }
         public int Role { get; set; }
-        public string Password { get; set; }
     }
 }
